44771decc2a6c4088dd4d73ef7b513983c82a103
cnikzad <cnikzad@lion.lmu.edu>
2025-12-09 14:48:58 -0800

    practice reset

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-practice using git restore 
+practice using git reset which delets a commit from the memory
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
