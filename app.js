0c7d3997d18d43f37d4ed2ea5e4206da9edd3cc0
cnikzad <cnikzad@lion.lmu.edu>
2025-12-08 19:02:24 -0800

    practice

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+practice using git restore 
+
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
